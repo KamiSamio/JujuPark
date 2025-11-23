9377324ff48080ebf0b8ad0cd3fe1c0f01993a8d
KamiSamio <NewSamuelMasse@proton.me>
2025-11-23 11:14:46 -0500

    Add files via upload
    V1.2

---
+++ db/animaux.sql
@@ -0,0 +1,119 @@
+create table animaux (
+  id integer primary key,
+  description varchar(500),
+  courriel varchar(80),
+  adresse varchar(75),
+  ville varchar(75),
+  cp varchar(7)
+);
+
+insert into animaux values (1, 'Parkomètre devant l’hôtel de ville.', 'H2X3L2@Parko.Metre.org', '4744 Rue Marquette', 'Montréal', 'H2X 3L2');
+insert into animaux values (2, 'Parkomètre près du marché public.', 'G1R4P5@Parko.Metre.org', '52 Avenue Cartier', 'Québec', 'G1R 4P5');
+insert into animaux values (3, 'Parkomètre à côté du centre communautaire.', 'J4H1A7@Parko.Metre.org', '213 Boulevard Taschereau', 'Longueuil', 'J4H 1A7');
+insert into animaux values (4, 'Parkomètre devant la bibliothèque municipale.', 'H3B2Y7@Parko.Metre.org', '1000 Rue Sainte-Catherine Ouest', 'Montréal', 'H3B 2Y7');
+insert into animaux values (5, 'Parkomètre près du centre sportif.', 'J7V3R4@Parko.Metre.org', '24 Rue Notre-Dame', 'Vaudreuil-Dorion', 'J7V 3R4');
+insert into animaux values (6, 'Parkomètre devant le musée local.', 'G8T5M1@Parko.Metre.org', '89 Rue Laviolette', 'Trois-Rivières', 'G8T 5M1');
+insert into animaux values (7, 'Parkomètre à l’entrée du stationnement municipal.', 'J8X2M9@Parko.Metre.org', '15 Rue Laurier', 'Gatineau', 'J8X 2M9');
+insert into animaux values (8, 'Parkomètre près du parc central.', 'H1V3R1@Parko.Metre.org', '3890 Rue Sherbrooke Est', 'Montréal', 'H1V 3R1');
+insert into animaux values (9, 'Parkomètre devant l’aréna.', 'J2S3B1@Parko.Metre.org', '122 Rue King Ouest', 'Saint-Hyacinthe', 'J2S 3B1');
+insert into animaux values (10, 'Parkomètre à proximité du quai.', 'G5R3Z7@Parko.Metre.org', '401 Rue Lafontaine', 'Rivière-du-Loup', 'G5R 3Z7');
+
+insert into animaux values (11, 'Parkomètre devant la gare routière.', 'H9S5W2@Parko.Metre.org', '12 Avenue Donegani', 'Pointe-Claire', 'H9S 5W2');
+insert into animaux values (12, 'Parkomètre près du terminus d’autobus.', 'J6Y2E4@Parko.Metre.org', '55 Boulevard Saint-Charles', 'Terrebonne', 'J6Y 2E4');
+insert into animaux values (13, 'Parkomètre à l’angle de la rue principale.', 'G7H4A3@Parko.Metre.org', '91 Rue Racine Est', 'Chicoutimi', 'G7H 4A3');
+insert into animaux values (14, 'Parkomètre devant le centre commercial.', 'H7N1G8@Parko.Metre.org', '245 Boulevard Saint-Martin Ouest', 'Laval', 'H7N 1G8');
+insert into animaux values (15, 'Parkomètre près du cinéma municipal.', 'J9H3C5@Parko.Metre.org', '64 Rue Principale', 'Aylmer', 'J9H 3C5');
+insert into animaux values (16, 'Parkomètre à côté de la mairie.', 'J0L1C0@Parko.Metre.org', '4744 Rue Marquette', 'Contrecoeur', 'J0L 1C0');
+insert into animaux values (17, 'Parkomètre devant l’école primaire.', 'G6P4R2@Parko.Metre.org', '98 Rue Laurier', 'Victoriaville', 'G6P 4R2');
+insert into animaux values (18, 'Parkomètre près du centre médical.', 'H4B1T4@Parko.Metre.org', '250 Avenue Somerled', 'Montréal', 'H4B 1T4');
+insert into animaux values (19, 'Parkomètre à proximité du parc municipal.', 'J3Y8A9@Parko.Metre.org', '344 Boulevard Curé-Poirier', 'Longueuil', 'J3Y 8A9');
+insert into animaux values (20, 'Parkomètre devant la caserne de pompiers.', 'J6A3S1@Parko.Metre.org', '22 Rue Saint-Louis', 'Repentigny', 'J6A 3S1');
+
+insert into animaux values (21, 'Parkomètre près de la patinoire extérieure.', 'H1W2K3@Parko.Metre.org', '3050 Rue Ontario Est', 'Montréal', 'H1W 2K3');
+insert into animaux values (22, 'Parkomètre devant le bureau de poste.', 'G9A4S5@Parko.Metre.org', '63 Rue Saint-Joseph', 'Trois-Rivières', 'G9A 4S5');
+insert into animaux values (23, 'Parkomètre à l’entrée du complexe sportif.', 'J8P1R9@Parko.Metre.org', '280 Boulevard de la Gappe', 'Gatineau', 'J8P 1R9');
+insert into animaux values (24, 'Parkomètre près du parc des jeunes.', 'H3C2N1@Parko.Metre.org', '100 Rue Peel', 'Montréal', 'H3C 2N1');
+insert into animaux values (25, 'Parkomètre devant l’hôtel municipal.', 'J7E3M4@Parko.Metre.org', '8 Rue Saint-Louis', 'Saint-Eustache', 'J7E 3M4');
+insert into animaux values (26, 'Parkomètre près du centre des congrès.', 'G1R5S7@Parko.Metre.org', '200 Rue Saint-Jean', 'Québec', 'G1R 5S7');
+insert into animaux values (27, 'Parkomètre à côté du théâtre.', 'H2Y1N9@Parko.Metre.org', '65 Rue Saint-Paul Ouest', 'Montréal', 'H2Y 1N9');
+insert into animaux values (28, 'Parkomètre devant le poste de police.', 'J3L3C2@Parko.Metre.org', '19 Rue Principale', 'Chambly', 'J3L 3C2');
+insert into animaux values (29, 'Parkomètre près du port de plaisance.', 'G5A2T3@Parko.Metre.org', '44 Rue du Quai', 'La Malbaie', 'G5A 2T3');
+insert into animaux values (30, 'Parkomètre à l’entrée du centre-ville.', 'J2K4S8@Parko.Metre.org', '203 Rue du Dépôt', 'Granby', 'J2K 4S8');
+
+insert into animaux values (31, 'Parkomètre devant le restaurant municipal.', 'J6W1A9@Parko.Metre.org', '109 Rue Saint-Joseph', 'Joliette', 'J6W 1A9');
+insert into animaux values (32, 'Parkomètre près du collège.', 'H8N2S3@Parko.Metre.org', '45 Rue Lapierre', 'Montréal', 'H8N 2S3');
+insert into animaux values (33, 'Parkomètre à proximité du marché fermier.', 'J8Y3G1@Parko.Metre.org', '92 Rue Montcalm', 'Gatineau', 'J8Y 3G1');
+insert into animaux values (34, 'Parkomètre devant la salle communautaire.', 'J0P1J0@Parko.Metre.org', '16 Rue Saint-Jean', 'Les Cèdres', 'J0P 1J0');
+insert into animaux values (35, 'Parkomètre près du pont principal.', 'G6V5C2@Parko.Metre.org', '71 Rue Fraser', 'Lévis', 'G6V 5C2');
+insert into animaux values (36, 'Parkomètre devant le centre culturel.', 'J1H1H2@Parko.Metre.org', '17 Rue Wellington Sud', 'Sherbrooke', 'J1H 1H2');
+insert into animaux values (37, 'Parkomètre à côté du café du coin.', 'H2T1B8@Parko.Metre.org', '12 Avenue du Mont-Royal', 'Montréal', 'H2T 1B8');
+insert into animaux values (38, 'Parkomètre près du square central.', 'G7N4P9@Parko.Metre.org', '77 Rue de l’Hôtel-de-Ville', 'Jonquière', 'G7N 4P9');
+insert into animaux values (39, 'Parkomètre devant la banque nationale.', 'J3P4K6@Parko.Metre.org', '28 Rue Sainte-Anne', 'Sorel-Tracy', 'J3P 4K6');
+insert into animaux values (40, 'Parkomètre à proximité du supermarché.', 'J6S3E7@Parko.Metre.org', '35 Rue Saint-Jacques', 'Salaberry-de-Valleyfield', 'J6S 3E7');
+
+insert into animaux values (41, 'Parkomètre près du centre jeunesse.', 'H3S2L1@Parko.Metre.org', '22 Rue Côte-des-Neiges', 'Montréal', 'H3S 2L1');
+insert into animaux values (42, 'Parkomètre devant le gymnase municipal.', 'J9T4P3@Parko.Metre.org', '88 Rue Principale', 'Amos', 'J9T 4P3');
+insert into animaux values (43, 'Parkomètre près de la promenade du fleuve.', 'G5C1V3@Parko.Metre.org', '31 Rue Saint-Pierre', 'Baie-Comeau', 'G5C 1V3');
+insert into animaux values (44, 'Parkomètre à côté du bureau touristique.', 'J2G8E1@Parko.Metre.org', '70 Rue Principale', 'Granby', 'J2G 8E1');
+insert into animaux values (45, 'Parkomètre devant la pharmacie locale.', 'H4G3S9@Parko.Metre.org', '39 Rue Verdun', 'Montréal', 'H4G 3S9');
+insert into animaux values (46, 'Parkomètre près du parc des aînés.', 'J5W1R2@Parko.Metre.org', '16 Rue Duhamel', 'L’Assomption', 'J5W 1R2');
+insert into animaux values (47, 'Parkomètre à proximité du bureau municipal.', 'G0R3K0@Parko.Metre.org', '4 Rue Saint-Jean-Baptiste', 'Sainte-Marie', 'G0R 3K0');
+insert into animaux values (48, 'Parkomètre devant la clinique vétérinaire.', 'H2C1Z5@Parko.Metre.org', '82 Rue Fleury Est', 'Montréal', 'H2C 1Z5');
+insert into animaux values (49, 'Parkomètre près du carrefour giratoire.', 'J6T3L1@Parko.Metre.org', '55 Boulevard Lionel-Groulx', 'Sainte-Thérèse', 'J6T 3L1');
+insert into animaux values (50, 'Parkomètre devant le centre jeunesse.', 'G1V4N6@Parko.Metre.org', '12 Avenue Myrand', 'Québec', 'G1V 4N6');
+
+insert into animaux values (51, 'Parkomètre à côté du café municipal.', 'J7A2P1@Parko.Metre.org', '33 Rue du Parc', 'Lachenaie', 'J7A 2P1');
+insert into animaux values (52, 'Parkomètre devant le poste de taxi.', 'G8Z2B4@Parko.Metre.org', '28 Rue des Forges', 'Trois-Rivières', 'G8Z 2B4');
+insert into animaux values (53, 'Parkomètre près du centre communautaire.', 'J0K1B0@Parko.Metre.org', '9 Rue Saint-Paul', 'Berthierville', 'J0K 1B0');
+insert into animaux values (54, 'Parkomètre à proximité du restaurant du coin.', 'H9C2N5@Parko.Metre.org', '101 Rue Saint-Charles', 'Beaconsfield', 'H9C 2N5');
+insert into animaux values (55, 'Parkomètre devant le salon de coiffure.', 'J8L1T9@Parko.Metre.org', '65 Rue Laval', 'Gatineau', 'J8L 1T9');
+insert into animaux values (56, 'Parkomètre près de la fontaine du parc.', 'G7K2J8@Parko.Metre.org', '12 Rue Saint-Dominique', 'Saguenay', 'G7K 2J8');
+insert into animaux values (57, 'Parkomètre devant le dépanneur local.', 'J2B3W6@Parko.Metre.org', '58 Rue King Est', 'Drummondville', 'J2B 3W6');
+insert into animaux values (58, 'Parkomètre à côté de la patisserie.', 'H1T1P8@Parko.Metre.org', '245 Rue Beaubien', 'Montréal', 'H1T 1P8');
+insert into animaux values (59, 'Parkomètre près du marché du samedi.', 'J3V5K2@Parko.Metre.org', '19 Rue Saint-Charles', 'Saint-Lambert', 'J3V 5K2');
+insert into animaux values (60, 'Parkomètre devant la boutique locale.', 'G1P3V8@Parko.Metre.org', '41 Boulevard Pierre-Bertrand', 'Québec', 'G1P 3V8');
+
+insert into animaux values (61, 'Parkomètre à proximité du centre des loisirs.', 'H4L3P7@Parko.Metre.org', '72 Rue Dudemaine', 'Montréal', 'H4L 3P7');
+insert into animaux values (62, 'Parkomètre devant la boulangerie.', 'J7P4E3@Parko.Metre.org', '17 Rue du Marché', 'Blainville', 'J7P 4E3');
+insert into animaux values (63, 'Parkomètre près du terminus régional.', 'G9N2L6@Parko.Metre.org', '26 Boulevard des Chenaux', 'Trois-Rivières', 'G9N 2L6');
+insert into animaux values (64, 'Parkomètre devant le centre de loisirs.', 'J8V3B2@Parko.Metre.org', '94 Rue Montclair', 'Gatineau', 'J8V 3B2');
+insert into animaux values (65, 'Parkomètre à côté du bureau d’information.', 'J4B2A1@Parko.Metre.org', '60 Boulevard Marie-Victorin', 'Brossard', 'J4B 2A1');
+insert into animaux values (66, 'Parkomètre près du parc des familles.', 'G7X3C9@Parko.Metre.org', '11 Rue Saint-Cyrille', 'Alma', 'G7X 3C9');
+insert into animaux values (67, 'Parkomètre devant le café du centre-ville.', 'H2E1K1@Parko.Metre.org', '20 Rue Saint-Zotique', 'Montréal', 'H2E 1K1');
+insert into animaux values (68, 'Parkomètre à proximité de la mairie.', 'J5Z1C5@Parko.Metre.org', '30 Rue Saint-Jacques', 'Mascouche', 'J5Z 1C5');
+insert into animaux values (69, 'Parkomètre devant la poste municipale.', 'J2X3M8@Parko.Metre.org', '40 Rue du Roi', 'Saint-Jean-sur-Richelieu', 'J2X 3M8');
+insert into animaux values (70, 'Parkomètre près de la salle de spectacle.', 'G1K7E9@Parko.Metre.org', '88 Rue Saint-Vallier', 'Québec', 'G1K 7E9');
+
+insert into animaux values (71, 'Parkomètre devant le parc du souvenir.', 'H4A3C3@Parko.Metre.org', '15 Rue Sherbrooke Ouest', 'Montréal', 'H4A 3C3');
+insert into animaux values (72, 'Parkomètre à côté de la station-service.', 'J7T2R2@Parko.Metre.org', '51 Rue Laval', 'Deux-Montagnes', 'J7T 2R2');
+insert into animaux values (73, 'Parkomètre près du marché public.', 'G5B1N9@Parko.Metre.org', '42 Rue de la Reine', 'Baie-Saint-Paul', 'G5B 1N9');
+insert into animaux values (74, 'Parkomètre devant le parc municipal.', 'J3H6C7@Parko.Metre.org', '36 Rue Saint-Laurent', 'Boucherville', 'J3H 6C7');
+insert into animaux values (75, 'Parkomètre à proximité du centre sportif.', 'J6Z3B4@Parko.Metre.org', '27 Rue Saint-Joseph', 'Boisbriand', 'J6Z 3B4');
+insert into animaux values (76, 'Parkomètre devant la galerie d’art.', 'H3L2E1@Parko.Metre.org', '18 Rue Chabanel', 'Montréal', 'H3L 2E1');
+insert into animaux values (77, 'Parkomètre près du centre d’achat local.', 'J7M1C8@Parko.Metre.org', '75 Rue Saint-Georges', 'Saint-Jérôme', 'J7M 1C8');
+insert into animaux values (78, 'Parkomètre devant le bureau d’assurance.', 'G8K3S5@Parko.Metre.org', '62 Rue Dequen', 'Chicoutimi', 'G8K 3S5');
+insert into animaux values (79, 'Parkomètre à proximité de la promenade du lac.', 'J9X4N2@Parko.Metre.org', '22 Rue Perreault Est', 'Rouyn-Noranda', 'J9X 4N2');
+insert into animaux values (80, 'Parkomètre près du centre administratif.', 'J0K2P0@Parko.Metre.org', '9 Rue Saint-Laurent', 'Crabtree', 'J0K 2P0');
+insert into animaux values (81, 'Parkomètre devant la salle des fêtes.', 'H2J3C2@Parko.Metre.org', '45 Rue Rachel Est', 'Montréal', 'H2J 3C2');
+insert into animaux values (82, 'Parkomètre à côté de la station de métro.', 'H1Z2N4@Parko.Metre.org', '99 Boulevard Pie-IX', 'Montréal', 'H1Z 2N4');
+insert into animaux values (83, 'Parkomètre près du parc des vétérans.', 'J6K1T3@Parko.Metre.org', '17 Rue Saint-Pierre', 'L’Assomption', 'J6K 1T3');
+insert into animaux values (84, 'Parkomètre devant le bureau de notaire.', 'G1S2C5@Parko.Metre.org', '1200 Avenue Maguire', 'Québec', 'G1S 2C5');
+insert into animaux values (85, 'Parkomètre à proximité du marché du dimanche.', 'J7L2W7@Parko.Metre.org', '83 Rue Principale', 'Sainte-Thérèse', 'J7L 2W7');
+insert into animaux values (86, 'Parkomètre près du parc des citoyens.', 'H3N2K8@Parko.Metre.org', '41 Avenue du Parc', 'Montréal', 'H3N 2K8');
+
+insert into animaux values (87, 'Parkomètre devant la salle de danse.', 'J2K3R4@Parko.Metre.org', '29 Rue Principale', 'Granby', 'J2K 3R4');
+insert into animaux values (88, 'Parkomètre à côté du café étudiant.', 'G9P2L1@Parko.Metre.org', '11 Rue Laviolette', 'Trois-Rivières', 'G9P 2L1');
+insert into animaux values (89, 'Parkomètre près du quai principal.', 'G5A1E8@Parko.Metre.org', '23 Rue du Quai', 'La Malbaie', 'G5A 1E8');
+insert into animaux values (90, 'Parkomètre devant le bureau municipal.', 'J8H1K9@Parko.Metre.org', '56 Rue Principale', 'Saint-Sauveur', 'J8H 1K9');
+insert into animaux values (91, 'Parkomètre à proximité du centre communautaire.', 'H4R2E3@Parko.Metre.org', '300 Boulevard Gouin Ouest', 'Montréal', 'H4R 2E3');
+insert into animaux values (92, 'Parkomètre près du marché fermier.', 'J9E3N7@Parko.Metre.org', '13 Rue Principale Sud', 'Maniwaki', 'J9E 3N7');
+insert into animaux values (93, 'Parkomètre devant la station d’autobus.', 'G7P4T5@Parko.Metre.org', '9 Rue Price', 'Jonquière', 'G7P 4T5');
+insert into animaux values (94, 'Parkomètre à côté de la bibliothèque publique.', 'J3G4C1@Parko.Metre.org', '25 Rue Saint-Laurent', 'Beloeil', 'J3G 4C1');
+insert into animaux values (95, 'Parkomètre près de la piste cyclable.', 'H2L1T7@Parko.Metre.org', '120 Rue Ontario Est', 'Montréal', 'H2L 1T7');
+insert into animaux values (96, 'Parkomètre devant la maison des jeunes.', 'J7G3P2@Parko.Metre.org', '77 Rue du Collège', 'Bois-des-Filion', 'J7G 3P2');
+
+insert into animaux values (97, 'Parkomètre à proximité du centre hospitalier.', 'G6V4M9@Parko.Metre.org', '40 Rue Wolfe', 'Lévis', 'G6V 4M9');
+insert into animaux values (98, 'Parkomètre près du stade municipal.', 'H1A2P9@Parko.Metre.org', '75 Rue Notre-Dame Est', 'Montréal', 'H1A 2P9');
+insert into animaux values (99, 'Parkomètre devant la salle de conférence.', 'J5K1M8@Parko.Metre.org', '22 Rue Saint-Jacques', 'Repentigny', 'J5K 1M8');
+insert into animaux values (100, 'Parkomètre à côté du parc du ruisseau.', 'G7N1S6@Parko.Metre.org', '10 Rue Saint-Cyrille Est', 'Jonquière', 'G7N 1S6');
+
